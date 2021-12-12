209391f2591f655dbd4229449315c5bbb4a23963
kuangyang <kuangyang828@gmail.com>
2021-12-13 00:57:46 +0800

    登记收入/支出功能完成

--- src/FamilyAccount.java
+++ src/FamilyAccount.java
@@ -4,7 +4,7 @@ public class FamilyAccount {
         //初始金额10000
         int balance = 10000;
         //用于记录用户收入和支出详情
-        String details = "收支\t\t账户金额\t\t收支金额\t\t说 明";
+        String details = "收支\t\t账户金额\t\t收支金额\t\t说 明\n";
         while (isFlag) {
             System.out.println("-----------------家庭收支记账软件-----------------\n");
             System.out.println("                   1.收支明细");
@@ -20,10 +20,29 @@ public class FamilyAccount {
                     System.out.println("-----------------------------------------------");
                     break;
                 case '2':
-                    System.out.println("登记收入");
+                    System.out.print("本次收入金额：");
+                    int addMoney = Utility.readNumber();
+                    System.out.print("本次支出说明：");
+                    String addInfo = Utility.readString();
+                    System.out.println("-------------------登记完成---------------------");
+                    //处理收支明细
+                    balance += addMoney;
+                    details += "收入\t\t" + balance + "\t\t" + addMoney + "\t\t" + addInfo + "\n";
                     break;
                 case '3':
-                    System.out.println("登记支出");
+                    System.out.print("本次支出金额：");
+                    int reduceMoney = Utility.readNumber();
+                    System.out.print("本次支出说明：");
+                    String reduceInfo = Utility.readString();
+                    System.out.println("-------------------登记完成---------------------");
+                    if (reduceMoney>balance){
+                        //支出大于余额，支付失败
+                        System.out.println("余额不足，支付失败！");
+                    }else {
+                        //处理收支明细
+                        balance -= reduceMoney;
+                        details += "支出\t\t" + balance + "\t\t" + reduceMoney + "\t\t" + reduceInfo + "\n";
+                    }
                     break;
                 case '4':
                     System.out.print("确认是否退出(Y/N)");
